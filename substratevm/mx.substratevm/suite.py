555ab7ff61478c3f0c17d1629b37342444162d49
dominik.mascherbauer@oracle.com <dominik.mascherbauer@oracle.com>
2024-07-29 16:19:34 +0200

    Create Initial Runtime Debug Info Implementation

--- substratevm/mx.substratevm/suite.py
+++ substratevm/mx.substratevm/suite.py
@@ -336,6 +336,7 @@ suite = {
             "dependencies": [
                 "com.oracle.svm.common",
                 "com.oracle.svm.shaded.org.objectweb.asm",
+                "com.oracle.objectfile",
                 "SVM_CONFIGURE",
             ],
             "requires" : [
@@ -1030,6 +1031,28 @@ suite = {
                 },
             },
             "jacoco" : "exclude",
+        },
+
+        "com.oracle.svm.native.debug": {
+            "native": "static_lib",
+            "subDir": "src",
+            "os_arch": {
+                "solaris": {
+                    "<others>": {
+                        "ignore": "solaris is not supported",
+                    },
+                },
+                "windows": {
+                    "<others>": {
+                        "cflags": ["-W4", "-O2", "-Zi"],
+                    },
+                },
+                "<others>": {
+                    "<others>": {
+                        "cflags": ["-Wall", "-fPIC", "-O2", "-g", "-gdwarf-4"],
+                    },
+                },
+            },
         },
 
         "svm-jvmfuncs-fallback-builder": {
@@ -1943,6 +1966,7 @@ suite = {
                 "com.oracle.objectfile.io",
                 "com.oracle.objectfile.debuginfo",
                 "com.oracle.objectfile.macho",
+                "com.oracle.objectfile.runtime",
               ],
 
               "requiresConcealed" : {
@@ -2072,6 +2096,7 @@ suite = {
                             "dependency:com.oracle.svm.native.libchelper/*",
                             "dependency:com.oracle.svm.native.jvm.posix/*",
                             "dependency:com.oracle.svm.native.libcontainer/*",
+                            "dependency:com.oracle.svm.native.debug/*",
                         ],
                     },
                 },
@@ -2080,9 +2105,11 @@ suite = {
                         # on all other os's we don't want libc specific subdirectories
                         "include/": [
                             "dependency:com.oracle.svm.native.libchelper/include/*",
+                            "dependency:com.oracle.svm.native.debug/include/*",
                         ],
                         "<os>-<arch>/": [
                             "dependency:com.oracle.svm.native.libchelper/<os>-<arch>/default/*",
+                            "dependency:com.oracle.svm.native.debug/<os>-<arch>/default/*",
                             "dependency:com.oracle.svm.native.jvm.posix/<os>-<arch>/default/*",
                             "dependency:com.oracle.svm.native.darwin/*",
                             "dependency:com.oracle.svm.native.jvm.windows/*",
